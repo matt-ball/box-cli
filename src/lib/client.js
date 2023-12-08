54f6f7face15c38cc190d91f5ed89197f6b2312a
Matt Ball <matt@matt-ball.co.uk>
2023-12-08 11:27:23 -0800

    fix first time setup

--- src/lib/client.js
+++ src/lib/client.js
@@ -2,9 +2,11 @@ const config = require('./config')
 const BoxSDK = require('box-node-sdk')
 
 function client () {
-  const cfg = config.get()
-  const box = new BoxSDK(cfg.clientCreds)
-  return box.getPersistentClient(cfg.tokenInfo)
+  try {
+    const cfg = config.get()
+    const box = new BoxSDK(cfg.clientCreds)
+    return box.getPersistentClient(cfg.tokenInfo)
+  } catch (e) {}
 }
 
 module.exports = client()
