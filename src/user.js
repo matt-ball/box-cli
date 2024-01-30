2c2e8d754cb4d7a9f865224ba843dffcd02dc285
Matt Ball <matt@matt-ball.co.uk>
2024-01-30 10:33:58 -0800

    Update user to work like other commands

--- src/user.js
+++ src/user.js
@@ -1,8 +1,14 @@
 const client = require('./lib/client')
 
-async function user () {
-  const user = await client.users.get(client.CURRENT_USER_ID)
-  console.log(`You are logged in as ${user.name} (${user.login})`)
+const operations = {
+  get: async () => {
+    const user = await client.users.get(client.CURRENT_USER_ID)
+    console.log(`You are logged in as ${user.name} (${user.login})`)
+  }
+}
+
+function user () {
+  operations.get()
 }
 
 module.exports = user
