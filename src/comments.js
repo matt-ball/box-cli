d4e4d39160ac316616f100c66af5bf60c80e6d66
Matt Ball <matt@matt-ball.co.uk>
2024-02-26 15:26:10 -0800

    add comment ability

---
+++ src/comments.js
@@ -0,0 +1,31 @@
+const client = require('./lib/client')
+
+const operations = {
+  get: async (commentID) => {
+    const comment = await client.comments.get(CommentID)
+    console.log(comment)
+  },
+  create: async (fileId, { message }) => {
+    const comment = await client.comments.create(fileId, message)
+    console.log(comment)
+  },
+  delete: async (commentID) => {
+    await client.comments.delete(commentID)
+    console.log('Comment deleted')
+  },
+  update: async (commentID, { message }) => {
+    const comment = await client.comments.update(commentID, message)
+    console.log(comment)
+  },
+  reply: async (commentID, { message }) => {
+    const comment = await client.comments.reply(commentID, message)
+    console.log(comment)
+  }
+}
+
+function comments (arg, options, subCommand) {
+  const operation = operations[subCommand._name]
+  operation(arg, options)
+}
+
+module.exports = comments
