12057ce04ec04ceddd9946b836e0f0b39cbe4c33
Matt Ball <matt@matt-ball.co.uk>
2024-03-11 11:34:26 -0700

    stop tests clashing with demos

--- test/file.test.js
+++ test/file.test.js
@@ -5,14 +5,14 @@ const path = require('path')
 let id
 
 test('can upload a file', async () => {
-  const filepath = path.join(__dirname, './data/logo.png')
+  const filepath = path.join(__dirname, './data/img.png')
   const result = await file(filepath, { folder: '0' }, { _name: 'upload' })
   // only files have entries that we must inspect
   // other methods can do result.id for example
   const data = result.entries[0]
   id = data.id
   
-  expect(data.name).toBe('logo.png')
+  expect(data.name).toBe('img.png')
 })
 
 test('can get a file', async () => {
@@ -22,9 +22,9 @@ test('can get a file', async () => {
 })
 
 test('can download a file', async () => {
-  const result = await file(id, { path: '/tmp/logo.png' }, { _name: 'download' })
+  const result = await file(id, { path: '/tmp/img.png' }, { _name: 'download' })
   
-  expect(fs.existsSync('/tmp/logo.png')).toBe(true)
+  expect(fs.existsSync('/tmp/img.png')).toBe(true)
 })
 
 test('can delete a file', async () => {
